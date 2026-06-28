8fe339d41e5325ec6a11e177f0e659a8cafe0f6f
hrems-dev <hrems077@gmail>
2026-06-28 16:12:09 -0500

    agregando datos principales

---
+++ src/main/java/pe/edu/upeu/sys_api_restaurant/SysApiRestaurantApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.sys_api_restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysApiRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysApiRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/upeu/sys_api_restaurant/SysApiRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.sys_api_restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysApiRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
